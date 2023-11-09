aa38fed5d073d337590245860660de139a523f9e
Vadym Vovk <vdm.wolf@gmail.com>
2023-11-09 19:55:47 +0200

    add BeanNameGenerator, BeanDefinitionRegistry interfaces

---
+++ src/main/java/com/petros/bringframework/beans/factory/BeanDefinitionRegistry.java
@@ -0,0 +1,21 @@
+package com.petros.bringframework.beans.factory;
+
+import com.petros.bringframework.beans.factory.config.BeanDefinition;
+
+public interface BeanDefinitionRegistry {
+
+    void registerBeanDefinition(String beanName, BeanDefinition beanDefinition);
+
+    void removeBeanDefinition(String beanName);
+
+    BeanDefinition getBeanDefinition(String beanName);
+
+    boolean containsBeanDefinition(String beanName);
+
+    String[] getBeanDefinitionNames();
+
+    int getBeanDefinitionCount();
+
+    boolean isBeanNameInUse(String beanName);
+
+}
