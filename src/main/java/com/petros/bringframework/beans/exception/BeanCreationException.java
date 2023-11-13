5b180bfb8e9021498cc5cfebfebd8aaedd89261f
OleksiiSkachkov <skachkov@kodaris.com>
2023-11-13 19:54:34 +0200

    BF-6: fix BeanCreationException

--- src/main/java/com/petros/bringframework/beans/exception/BeanCreationException.java
+++ src/main/java/com/petros/bringframework/beans/exception/BeanCreationException.java
@@ -1,11 +1,13 @@
 package com.petros.bringframework.beans.exception;
 
 
+import com.petros.bringframework.beans.FatalBeanException;
+
 /**
  * Exception thrown when a BeanFactory encounters an error when attempting
  * to create a bean from a bean definition.
  */
-public class BeanCreationException extends RuntimeException {
+public class BeanCreationException extends FatalBeanException {
     public BeanCreationException(Class<?> clazz, String message, Throwable e) {
         super("Class " + clazz.getName() + " " + message, e);
     }
